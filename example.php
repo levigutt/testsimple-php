6c2b62fa1a05c64eeb938b68f3c3b82c08f8a07f
levi <levinj@pm.me>
2023-08-25 01:40:57 +0200

    add: composer setup with autoload

--- example.php
+++ example.php
@@ -1,9 +1,11 @@
 <?php
-use TestSimple;
+require_once "vendor/autoload.php";
 
+use TestSimple\Tester as Tester;
 $test = new Tester();
 
-$test->ok(1 + 1 == 2,           "1 plus 1 equals 2");
-$test->ok(length("mario") == 5, "Mario´s name is 5 letters long");
+$test->ok(1 + 1 == 2, "1 plus 1 equals 2");
+
+$test->ok(4 == strlen("test"), "TEST is a four letter word");
 
 $test->done();
