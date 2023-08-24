88b1f2da5f75886b04e8ef1bedac88e92eed9e09
levi <levinj@pm.me>
2023-08-25 01:51:20 +0200

    improve example

--- example.php
+++ example.php
@@ -1,11 +1,15 @@
 <?php
 require_once "vendor/autoload.php";
 
-use TestSimple\Tester as Tester;
+use TestSimple\Tester;
 $test = new Tester();
 
 $test->ok(1 + 1 == 2, "1 plus 1 equals 2");
 
 $test->ok(4 == strlen("test"), "TEST is a four letter word");
+
+$test->ok("1" == 1); # description is not required
+
+$test->ok(2 + 2 == 5, "2 plus 2 equals 5");
 
 $test->done();
