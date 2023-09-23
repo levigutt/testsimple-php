2ca47a52794b1b3d25e3d9f4bbeba335edc712f3
levi <levinj@pm.me>
2023-09-24 00:28:56 +0200

    clean up examples

--- example/standalone.php
+++ example/standalone.php
@@ -1,12 +1,12 @@
 <?php
 
 require_once("testsimple.php");
-$test = new TestSimple\Tester();
+$assert = new TestSimple\Assert();
 
-$test->ok(false);
-$test->ok(1 + 1 == 2,     "1 plus 1 equals 2"); # optional description
-$test->insist(2 + 2 == 5, "2 plus 2 equals 5"); # stop execution on failure
-$test->ok(42%5);
-$test->done();
+$assert->ok(false);
+$assert->ok(1 + 1 == 2,     "1 plus 1 equals 2"); # optional description
+$assert->insist(2 + 2 == 5, "2 plus 2 equals 5"); # stop execution on failure
+$assert->ok(42%5);
+$assert->done();
 
 ?>
--- example/tests/example.php
+++ example/tests/example.php
@@ -1,13 +1,13 @@
 <?php
 
-if( !$test )
+if( !$assert )
     die("Run via prove.php\n");
 
-$test->ok(1 + 1 == 2, "1 plus 1 equals 2");
+$assert->ok(1 + 1 == 2, "1 plus 1 equals 2");
 
-$test->ok(4 == strlen("test"), "TEST is a four letter word");
+$assert->ok(4 == strlen("test"), "TEST is a four letter word");
 
-$test->ok("1" == 1); # description is not required
+$assert->ok("1" == 1); # description is not required
 
-$test->ok(2 + 2 == 5, "2 plus 2 equals 5");
+$assert->ok(2 + 2 == 5, "2 plus 2 equals 5");
 
