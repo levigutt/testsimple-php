bc709cad897832d6651dced1335f209d6d3d9ca0
levigutt <16719598+levigutt@users.noreply.github.com>
2023-09-23 23:31:50 +0200

    add: tests to ensure correct test file order (#24)
    Co-authored-by: levi <levinj@pm.me>

--- t/02-false.php
+++ t/02-false.php
@@ -1,9 +1,12 @@
 <?php
 
-$assert->plan+= 5;
+$assert->plan+= 6;
 
 $assert->not_ok(false, "false should fail");
 $assert->not_ok(0,     "0 should fail");
 $assert->not_ok(null,  "null should fail");
 $assert->not_ok('',    "'' should fail");
 $assert->not_ok([],    "[] should fail");
+
+$test_file_order++;
+$assert->ok(2 == $test_file_order, "file #2 comes second");
