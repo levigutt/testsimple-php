8115aeed25e97afb186195df49655ca72b4c5899
levi <levinj@pm.me>
2023-10-02 12:12:12 +0200

    improve examples

--- example/tests/example.php
+++
@@ -1,9 +0,0 @@
-<?php
-isset( $assert ) or die("Run tests via prove.php\n");
-
-$assert->is(2, 1+1, "basic math works");
-
-$assert->is(4, strlen("TEST"), "TEST is a four letter word");
-
-$assert->is(5, 2+2, "2 plus 2 equals 5");
-
--- example/tests/more-tests.php
+++ example/tests/more-tests.php
@@ -1,5 +1,4 @@
 <?php
-isset( $assert ) or die("Run tests via prove.php\n");
 
 $assert->ok("truthy", "absolute truth is not always needed");
 
